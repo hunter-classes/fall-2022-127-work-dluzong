2272cd4e7d16ff958122ba11d08fb374642570bd
daphne <102302061+dluzong@users.noreply.github.com>
2022-09-01 12:48:08 +0000

    added code and assignments folder

---
+++ code/turtles/t1.py
@@ -0,0 +1,16 @@
+import turtle
+
+wn = turtle.Screen()    #screen makes a window
+
+crush = turtle.Turtle()    #makes a turtle
+
+crush.forward(50)
+crush.left(30)
+crush.forward(130)
+crush.right(50)
+crush.forward(70)
+
+wn.exitonclick()
+wn.mainloop()
+
+a = 10 +30
