4c02d415fc2191efc17e1bda74698fd6fcd3b495
daphne <102302061+dluzong@users.noreply.github.com>
2022-09-08 13:44:11 +0000

    modified turtles-functions.py, added triangle

--- code/turtles/turtles-functions.py
+++ code/turtles/turtles-functions.py
@@ -2,7 +2,7 @@ import turtle
 
 
 def square(t, x, y, w, color, sidelen):
-  """
+    """
   Draw a square using the turtle passed into t
   Parameters:
     t - a turtle
@@ -14,25 +14,50 @@ def square(t, x, y, w, color, sidelen):
     nothing
   
   """
-  #set location, color, and width
-  t.penup()
-  t.goto(x, y)
-  t.width(w)
-  t.color(color)
-  t.pendown()
-  #draw a square
-  for i in range(4):
-      t.forward(sidelen)
-      t.right(90)
+    #set location, color, and width
+    t.penup()
+    t.goto(x, y)
+    t.width(w)
+    t.color(color)
+    t.pendown()
+    #draw a square
+    for i in range(4):
+        t.forward(sidelen)
+        t.right(90)
+
 
 #def triangle(fill in these):
-  #code to draw the triangle
+#code to draw the triangle
+def triangle(t, x, y, w, color, sidelen):
+    """
+  Draw a triangle using the turtle passed into t
+  Parameters:
+    t - a turtle
+    x,y - location
+    w - width of side
+    color - color to draw in
+    sidelen - length of each side
+  Returns:
+    nothing
+  
+  """
+    #set location, color, and width
+    t.penup()
+    t.goto(x, y)
+    t.width(w)
+    t.color(color)
+    t.pendown()
+    #draw a triangle
+    for i in range(3):
+        t.forward(sidelen)
+        t.right(120)
+
 
 #def hexagon(fill in these):
-  #code to draw the hexagon
-    
+#code to draw the hexagon
+
 #def ngon(t,numsides,x,y,color,width,sidelen):
-  #code to draw the ngon
+#code to draw the ngon
 
 wn = turtle.Screen()
 
@@ -45,6 +70,8 @@ square(squirt, 100, 100, 5, "red", 80)
 square(crush, -50, 30, 3, "yellow", 100)
 crush.setheading(45)
 square(crush, 150, 30, 2, "blue", 60)
+
+triangle(crush, -100, -50, 2, "green", 50)
 
 wn.exitonclick()
 wn.mainloop()
