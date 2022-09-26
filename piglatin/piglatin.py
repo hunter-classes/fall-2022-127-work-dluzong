fc295d2faf9c3c2a32f2f472068da86c0b0729d3
daphne <102302061+dluzong@users.noreply.github.com>
2022-09-26 17:40:25 +0000

    added fizzbuzz

--- piglatin/piglatin.py
+++ piglatin/piglatin.py
@@ -1,5 +1,10 @@
 #piglatin
-def piglatin(word):
+#TO-DO:
+#1. make it work for capitalized words
+  #ex: Cable -> Ablecay
+#2. Handle periods (and possible other punctuation)
+  #ex: Able. --> Able.ay and cable. --> ablecay.
+def piglatinify(word):
     """
 input: a string representing a word
 returns: a new string with the word converted to piglatin
@@ -14,34 +19,43 @@ try to also handle upper case words
 
 """
     vowel = ('A', 'E', 'I', 'O', 'U')
-    consonant = ('B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P',
-                 'Q', 'R', 'S', 'T', 'Y', 'V', 'X', 'Z')
+  #can also do if first == "A" or first == 'E' or ... 
+  #even better: if first in 'aeiou'
+    consonant = ('B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P','Q', 'R', 'S', 'T', 'Y', 'V', 'X', 'Z')
     firstLetter = word[0]
     firstLetter = str(firstLetter)
     firstLetter = firstLetter.upper()
 
     if firstLetter in vowel:
-        return word + "yay"
+    #if firstLetter in 'aeiou':
+      result = word + "yay"
 
     elif firstLetter in consonant:
         lenOfWord = len(word)
-        otherLetters = word[1:lenOfWord]
+        otherLetters = word[2:lenOfWord]
         firstLetter = firstLetter.lower()
+        secondLetter = word[1]
+        secondLetter = secondLetter.upper()
+        result = secondLetter + otherLetters + firstLetter + "ay"
+        # or result = word[1:]+first+'ay'
+    return result
 
-        return otherLetters + firstLetter + "ay"
+test_word = "Hello"
+result = piglatinify(test_word)
+print(test_word, "-->", result)
 
-
-print(piglatin(input("What word do you want to translate to pig latin?")))
-
+test_word = "Able"
+result = piglatinify(test_word)
+print(test_word, "-->", result)
 
 #bondify
-def bondify(name):
-    """
-    input: a string in the form "first last"
-    returns: a string in the form "Last, First Last"
-    """
-    loc = name.find(" ")
-    return name[loc:] + ", " + name[0:loc] + " " + name[loc:]
+#def bondify(name):
+    #"""
+    #input: a string in the form "first last"
+    #returns: a string in the form "Last, First Last"
+    #"""
+   # loc = name.find(" ")
+   # return name[loc:] + ", " + name[0:loc] + " " + name[loc:]
 
 
-print(bondify("james bond"))
+#print(bondify("james bond"))
