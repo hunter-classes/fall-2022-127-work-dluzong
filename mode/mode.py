7fba53a6627c394d70feea3b0b56f90cc64ec38c
daphne <102302061+dluzong@users.noreply.github.com>
2022-10-27 12:43:05 +0000

    added random list function

--- mode/mode.py
+++ mode/mode.py
@@ -1,6 +1,7 @@
 #findLargest(l) which takes in a list of numbers and returns the value of the smallest number
 
 #freq(l,v) which takes a list of numbers (l) and a value (v). The function will return the frequency of v, that is, the number of times that v appears in l.
+import random
 def findLargest(l):
   largestNum = l[0]
   for num in l[1:]:
@@ -16,6 +17,14 @@ def freq(l,v):
     if num == v:
       n += 1
   return n
+def buildRandomList(size,maxvalue):
+  #result = []
+  #for x in range(size):
+    #result.append(random.randrange(maxvalue))
+  #return result
+  result = [random.randrange(maxvalue) for x in range(size)]
+  return result
 
+  
 print(freq(['1','2','2','3','4','5','5','2','6'],'2'))
     
