663a782a1e49e314b89c2168d140208736b3a274
daphne <102302061+dluzong@users.noreply.github.com>
2022-10-31 13:36:26 +0000

    elaborated madlibs code

--- project-01-madlibs/madlibs.py
+++ project-01-madlibs/madlibs.py
@@ -1,18 +1,29 @@
 import random
-sample_sentence = 'Sam <VERB> the <NOUN> and then <VERB> the <NOUN> later.'
-verbs = ['ate','drank','walked']
-nouns = ['dog','hammer','cat','car','frog']
+sample_sentence = 'Today there was a <ADJECTIVE> <ANIMAL> who <ADVERB> woke up very hungry. They had an appetite for <FOOD> and searched eventually finding it behind a <NOUN>'
+adjectives = ['beautiful','ugly','cute','silly','sparkly']
+animals = ['dog','dragon','cat','unicorn','bunny']
+adverbs = ['quickly','eagerly','slowly','happily','groggily']
+foods = ['cheese','lasagna','breadsticks','lettuce','steak']
+nouns = ['can of beans','car','frog', 'rock','tree']
 split_sample_sentence = sample_sentence.split()
 
-new_madlib = []
+x = 0
+while x < 2:
+  new_madlib = []
+  for word in split_sample_sentence:
+    if word == "<ADJECTIVE>":
+      word = word.replace("<ADJECTIVE>", str(random.choice(adjectives)))
+    elif word == "<ANIMAL>":
+      word = word.replace("<ANIMAL>", str(random.choice(animals)))
+    elif word == "<ADVERB>":
+      word = word.replace("<ADVERB>", str(random.choice(adverbs)))
+    elif word == "<FOOD>":
+      word = word.replace("<FOOD>", str(random.choice(foods)))
+    elif word == "<NOUN>":
+      word = word.replace("<NOUN>", str(random.choice(nouns)))
 
-for word in split_sample_sentence:
-  if word == "<VERB>":
-    word = word.replace("<VERB>", str(random.choice(verbs)))
-  elif word == "<NOUN>":
-    word = word.replace("<NOUN>", str(random.choice(nouns)))
-  new_madlib.append(word)
-
-new_madlib = ' '.join(new_madlib)
-  
-print(new_madlib)
+    new_madlib.append(word)
+  new_madlib = ' '.join(new_madlib)  
+  print(new_madlib)
+  print(' ')
+  x = x+1
