ac33124775839a55e6041488641de191029b6ba1
daphne <102302061+dluzong@users.noreply.github.com>
2022-11-07 14:31:35 +0000

    added to dict and wordcounts

--- dictionary-demo.py
+++ dictionary-demo.py
@@ -12,6 +12,38 @@ print(d)
 
 person = {'first' : 'John',
          'last' : "Smith"
-         'age' : 50}
+         'age' : '50'}
 person['age'] = person['age'] + 1
 print(person)
+
+k = person.keys()
+print(k)
+for item in k:
+  print('person[',item,'] =',person[item])
+
+#convert the dict_keys thing into a list:
+klist = [x for x in person.keys()]
+print(klist)
+
+# pull out the values and convert to list
+vlist = [x for x in person.values()]
+
+a,b,c = yt
+
+for k,v in person.items():
+  print(k,v)
+
+for k in person.keys():
+  print(k,person[k])
+
+s1 = {'name':'barney','id':1,'scores':[100,95,85]}
+s2 = {'name':'jen','id':2,'scores':[95,85]}
+s3 = {'name':'marcel','id':3,'scores':[99,76,88,82]}
+s4 = {'name':'sookie','id':4,'scores':[98]}
+
+student_list = [s1,s2,s3,s4,s5,s6]
+student_dict = {}
+for item in student_list:
+  student_dict[item['id']] = item 
+
+print(x fpr x in student_list if x['gender']=='f')
