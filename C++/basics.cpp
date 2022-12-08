fe0aa100206d3ef9d0bca2746b821cda47221505
daphne <102302061+dluzong@users.noreply.github.com>
2022-12-08 13:45:10 +0000

    added folder for c++ files

---
+++ C++/basics.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+//int short long unsigned
+int main()
+{
+ // int x = 20;
+ // int y;
+ // int z;
+
+  //y = 30;
+  //int a = 10,b = 20;
+  int x = 21;
+  double c = 21;
+  char c;
+  bool tf = false;
+  std:: string s = "this is a string";
+  
+  std::cout << "Hello World!\n";
+  std::cout << "the var x = " << x << "\n";
+  std::cout << "Doubles:" <<x<<" "<< d/2 <<std:: emdl;
+  std::cout << "Ints:" <<x << " " x/2 << std::endl;
+   
+  c = 'x'; // a SINGLE ckaracter
+  std::cout <<c<< "\n"
+  std::cout <<"A true value:" <<tf <<1 
+  return 0;
+}
+
+//-wall : flags that tell how linux to behave i.e -l = long, 
+//-w is warning (wall = w all)
---
+++ C++/control.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+std::cout <<
+
+int a;
+a = 10;
+
+if (a<20){
+  std::cout <<"a  20\n";
+  std:::
+}
