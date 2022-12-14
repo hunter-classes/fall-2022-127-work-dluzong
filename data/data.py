1bd229da6cd9c5697d98897cf72bf242e96ee8ca
daphne <102302061+dluzong@users.noreply.github.com>
2022-12-14 07:57:18 +0000

    deleted test folder

--- data/data.py
+++ data/data.py
@@ -2,32 +2,15 @@
 #EXTRA: 
 
 import csv
-
+import panda as pd
 #opens and reads CSV file (starbucks)
 reader = csv.reader(open('sb-nutrition-drinks.csv'))
+reader2 = pd.reader_csv('sb-nutrition-foods.csv')
 
 #test if file opened correctly
-#for line in reader:
-#    print(line)
+#for line in reader2:
+    #print(line)
 
-cal = [] #make empty list for calorie nums
-#takes each row in data as a line, and adds calorie value to cal list
-for line in reader:
-  cal.append(line[1])
-cal.remove('Calories')
-#removes non numbers from list
-for item in cal:
-  if '-' in cal:
-    cal.remove('-')
 
-#calculates the average
-def calAvg(v):
-  sum_cal = 0
-  for item in cal:
-    sum_cal = sum_cal + int(item)
-  average = sum_cal / len(cal)
-  return average
-
-print("The average number of calories in Starbucks drinks is:", calAvg(cal))
     
   
