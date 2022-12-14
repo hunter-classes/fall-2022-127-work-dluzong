d8d3d3d35efc66d918a54894634229b33f9d4aa8
daphne <102302061+dluzong@users.noreply.github.com>
2022-12-14 04:02:45 +0000

    added data folder

---
+++ chapter-2/C++/namespace.cpp
@@ -0,0 +1,12 @@
+//This is better --explicitly
+//use specific functions
+//using std::cout;
+//using std::endl;
+//but best is just don't do either
+
+int main(){
+  std::cout << <<
+    std::cout << <<"\n";
+    std::cout <<"hello world" << td:endl;
+    return 0;
+}
